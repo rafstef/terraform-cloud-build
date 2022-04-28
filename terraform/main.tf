6162bb4ed7af8928378b9b40fbe39d03f9eb4cd4
Raffaele Stefanile <raffaele@Raffaeles-MacBook-Pro.local>
2022-04-28 17:54:28 +0200

    test

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,7 +3,8 @@ provider "google" {}
 resource "google_sql_database_instance" "master" {
     name = "master-instance"
     database_version = "POSTGRES_11"
-    region  = "eu-central1"
+    region  = "europe-central1"
+    provider = google-beta
     settings {
         tier = "db-f1-micro"
     }
